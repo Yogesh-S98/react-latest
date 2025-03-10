c67d3034c1aef697366352d5cf1589cad1ef1f9d
Yogesh-S98 <yogeswararao.s@imaginnovate.com>
2025-03-10 16:39:03 +0530

    remove

--- src/App.js
+++ src/App.js
@@ -201,9 +201,6 @@ function Home() {
     <div className="App-header">
       <Hooks></Hooks>
       <MyContext.Provider value={'Hello, World!'}>
-        {/* <div>new</div>
-        <h1>React App</h1>
-        <img src={logo} className="App-logo" alt="logo"/> */}
         <div>Open List</div>
         {/* <ListDiv></ListDiv>
         <TabView></TabView> */}
